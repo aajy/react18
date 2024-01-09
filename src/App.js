ac45b4eab6f9c2d08c1f89e59e7346f10b8dee4d
aajy <aajy.000@gmail.com>
2024-01-09 18:12:52 +0900

    scroll값을 이용해서 top버튼 나타나도록 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
+import { useEffect, useRef } from 'react';
 import Btns from './components/btns/Btns';
 import News from './components/news/News';
 import Pics from './components/pics/Pics';
 import Visual from './components/visual/Visual';
 import './global.scss';
+import Anime from './asset/anime';
 
 function App() {
-  return (
-    <div className="App">
-      <Visual />
-      <News />
-      <Pics />
-      <Btns />
-    </div>
-  );
+	const btnTop = useRef(null);
+	const handleScroll = () => {
+		const scroll = window.scrollY;
+		scroll >= 200
+			? btnTop.current.classList.add('on')
+			: btnTop.current.classList.remove('on');
+	};
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+	}, []);
+	return (
+		<div className='App'>
+			<Visual />
+			<News />
+			<Pics />
+			<Btns />
+			<button
+				ref={btnTop}
+				className='btnTop'
+				onClick={() => {
+					new Anime(window, { scroll: 0 });
+				}}
+			>
+				top
+			</button>
+		</div>
+	);
 }
 
 export default App;
